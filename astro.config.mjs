41e56013d3b936350ec5e73adaf8efd74dd37e3e
Rafael Díaz <rafandpufff@gmail.com>
2024-08-24 13:01:34 +0200

    Update astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,8 @@ const theme = {
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],  
+  integrations: [mdx()],
+  base:"./",
   // Change background color for the code Mdx theme
   markdown: {
     shikiConfig: {
